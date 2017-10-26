0b9e291f2a0e07cd011e88531f5ff39c407d1041
Sahasrangshu Guha <sahasrangshu.guha@ericsson.com>
2017-10-26 14:33:18 +0530

    changes

--- banner.js
+++ banner.js
@@ -1,7 +1,14 @@
 var bindClickEventsToNewsBlock,
     bindHooverEventsToNewsBlock,
     bindEvents,
-    openURL;
+    openURL,
+    applyCssForSliding,
+    getNumberOfNewsBlockPerPage,
+    slideLeft;
+
+getNumberOfNewsBlockPerPage = (function() {
+    return 9;
+});
 
 openURL = (function(url) {
     window.open(url);
@@ -30,24 +37,93 @@ bindEvents = (function(newsBlocks) {
     bindHooverEventsToNewsBlock(newsBlocks);
 });
 
-applyCssForSliding = (function() {
+applyCssForSliding = (function(numberOfNewsBlocksPerPage) {
+    var newsBlocks,
+        newsBlocksLength,
+        index,
+        windowWidth;
+
+    windowWidth = $(window).width();
+    newsBlocks = $(".banner-item-big, .banner-item-normal");
+    newsBlocksLength = newsBlocks.length;
+    // newsBlocks.eq(1).parent().css({
+    //     "position": "relative"
+    // });
+
+    index = 0;
+    newsBlocks.each(function() {
+        $(this).data('index', index);
+        if (index < numberOfNewsBlocksPerPage) {
+
+        } else {
+            //$(this).hide();
+            $(this).css({
+                "left": windowWidth * (parseInt(index / 9)) + "px"
+            });
+        }
+        index += 1;
+    });
+});
+
+var getLeftValueAsInteger = (function(leftValue) {
+    var value = parseInt(leftValue.split("px")[0]);
+    console.log(value);
+    return value;
+});
+
+slideLeft = (function() {
+    var newsBlocks,
+        newsBlocksLength,
+        windowWidth,
+        visibleNewsBlocks,
+        leftValueOfLastNewsBlock,
+        promiseArray;
+
+    promiseArray = [];
+    windowWidth = $(window).width();
+    newsBlocks = $(".banner-item-big, .banner-item-normal");
+
+    leftValueOfLastNewsBlock = newsBlocks.last().css("left");
+    //alert(leftValueOfLastNewsBlock);
+    newsBlocks.each(function() {
+        var element = $(this);
+        var promise = new Promise(function(resolve, reject) {
+            var left = getLeftValueAsInteger(element.css("left")) - windowWidth;
+            element.animate({
+                "left": left + "px"
+            }, 1000, function() {
+                resolve();
+            });
+        });
+        promiseArray.push(promise);
+    });
+
+    Promise.all(promiseArray, function() {
+        alert("done");
+    });
 
 });
 
 $(document).ready(function() {
-    var newsBlocks = $(".banner-item-big, .banner-item-normal");
+
+    var newsBlocks,
+        numberOfNewsBlocksPerPage;
+
+    numberOfNewsBlocksPerPage = getNumberOfNewsBlockPerPage();
+
+    newsBlocks = $(".banner-item-big, .banner-item-normal");
 
     bindEvents(newsBlocks);
 
     window.setTimeout(function() {
-        applyCssForSliding();
+        applyCssForSliding(numberOfNewsBlocksPerPage);
     }, 1000);
     var resizeTask = null;
     $(window).resize(function() {
         if (resizeTask == null) {
             resizeTask = window.setTimeout(function() {
-                alert("x");
-                applyCssForSliding();
+                numberOfNewsBlocksPerPage = getNumberOfNewsBlockPerPage();
+                applyCssForSliding(numberOfNewsBlocksPerPage);
             }, 1000);
         } else {
             clearTimeout(resizeTask);
@@ -55,5 +131,7 @@ $(document).ready(function() {
         }
 
     });
-
+    window.setInterval(function() {
+        slideLeft();
+    }, 5000);
 });
